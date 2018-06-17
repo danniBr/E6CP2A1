e98a3b7a19a4e449cd7f3fa7ea77c106dca19891
danniBr <danitza.br@gmail.com>
2018-06-16 21:28:56 -0400

    commit ejercicio 4

--- 1 metodos/ejercicio4.rb
+++ 1 metodos/ejercicio4.rb
@@ -1,3 +1,9 @@
 # Crear un método que muestre en pantalla un saludo, el método
 # debe recibir un parámetro, si ese parámetro es el string "Hola" el
 # método debe mostrar en pantalla "Hola Mundo".
+
+def saludo(param)
+  puts 'Hola Mundo' if param == 'hola'
+end
+
+saludo('hola')
