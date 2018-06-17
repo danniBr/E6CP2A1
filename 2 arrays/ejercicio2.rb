bf8c4de36dc52f96bf42302e714d9d58aadf81d6
danniBr <danitza.br@gmail.com>
2018-06-17 13:23:47 -0400

    commit ejercicio 2 arrays

--- 2 arrays/ejercicio2.rb
+++ 2 arrays/ejercicio2.rb
@@ -9,4 +9,31 @@
 # 5. Utilizando un arreglo vacío auxiliar y operaciones de push y pop:
 #    invertir el orden de los elementos en un arreglo.
 
-a = [1,2,3,9,1,4,5,2,3,6,6]
+a = [1, 2, 3, 9, 1, 4, 5, 2, 3, 6, 6]
+
+# 1:
+a.pop
+print "#{a} \n"
+
+# 2:
+a.shift
+print "#{a} \n"
+
+# 3:
+a.delete_at(4)
+print "#{a} \n"
+
+# 4:
+a.each_with_index do |ele, i|
+  a.pop unless ele == 1
+end
+print "#{a} \n"
+
+# 5:
+b = []
+
+a.each_with_index do |elem, i|
+  b.push(a.reverse[i])
+end
+
+print b
