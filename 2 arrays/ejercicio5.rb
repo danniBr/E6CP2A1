0b99baa402f92218ba4d35138c73cb1dcdbd85c9
danniBr <danitza.br@gmail.com>
2018-06-17 14:33:02 -0400

    ejercicio 5 arrays

--- 2 arrays/ejercicio5.rb
+++ 2 arrays/ejercicio5.rb
@@ -8,8 +8,9 @@ products = %w(Producto1 Producto2 Producto3 Producto4)
 prices = %w[1000 2000 1500 950]
 
 html = ''
-products.each do |i|
+products.zip(prices).each do |product, price|
   html += "<div class='product'>"
+  html += "<p>#{product} </p> <p>Precio: #{price} </p>"
   html += "</div>\n"
 end
 
