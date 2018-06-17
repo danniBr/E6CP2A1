380d06ad2fde76435b624c9bc02352865257b996
danniBr <danitza.br@gmail.com>
2018-06-16 21:17:02 -0400

    commit ejercicio 2

--- 1 metodos/ejercicio2.rb
+++ 1 metodos/ejercicio2.rb
@@ -1,15 +1,16 @@
-# El siguiente programa debería mostrar sí o no, sin embargo muestrar error
+# El siguiente programa debera mostrar sí o no, sin embargo muestrar error
 # Se pide identificar el error y corregirlo.
 
 def random
   result = [true, false].sample
   puts result
+  if result == true
+    puts 'sí'
+  elsif result == false
+    puts 'no'
+  else
+    puts 'error'
+  end
 end
 
-if random == true
-  puts 'sí'
-elsif random == false
-  puts 'no'
-else
-  puts 'error'
-end
+random
